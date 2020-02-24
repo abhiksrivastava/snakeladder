73bfc886d775565cb4d797d929a185e4cee7eb48
abhiksrivastava <abhik.srivastava10003@gmail.com>
2020-02-24 11:24:36 +0530

    [ADDED:ROLL]

--- snakeladder.sh
+++ snakeladder.sh
@@ -1,3 +1,11 @@
 #welcome to snake and ladder
 echo "----welcome to sanke and ladder---"
 echo " lets start the game "
+function roll()
+{
+   randomCheck=$((RANDOM%6 + 1))
+   echo "When we throw the die $randomCheck comes"
+   ((totalrandomCheck++))
+}
+
+
